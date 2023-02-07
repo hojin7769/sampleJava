6df52eaf0d81aad5c8ee3e0fa9d3f2e0a1844a0d
hohin <sungsill150@gmail.com>
2023-02-07 11:09:37 +0900

    1
    2

--- src/main/java/com/example/demo/api/testApi.java
+++ src/main/java/com/example/demo/api/testApi.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class testApi {
 	@GetMapping("/hello")
 	public String hello(){
-		return "hello Tuesday Spring API 응답111";
+		return "hello Tuesday Spring API 응답12";
 	};
 	
 	@Autowired
